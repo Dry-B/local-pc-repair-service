28023d99b0e60f4fe877ee61ea528d750654da12
Brendan <badrysdale01@gmail.com>
2024-01-09 15:22:37 -0500

    connecting server and initial components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
-import './App.css';
 import { useEffect } from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+} from 'react-router-dom';
+import { styled } from 'styled-components';
+import { Welcome } from './Components/Welcome';
+import { Main } from './Components/Main';
 
-function App() {
+const App = () => {
 	useEffect(() => {
 		fetch('/')
 			.then((res) => res.json())
-			.then((data) => console.log(data));
+			.then((data) => console.log(data.data))
+			.catch((error) => console.log(error));
 	}, []);
-	return <div className="App">App</div>;
-}
+
+	return (
+		<GlobalStyles>
+			<Router>
+				<Welcome />
+				<Routes>
+					<Route path="/" element={<Main />} />
+				</Routes>
+			</Router>
+		</GlobalStyles>
+	);
+};
+
+const GlobalStyles = styled.div`
+	text-align: center;
+`;
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 const port = 4000;
 
+app.use(morgan('tiny'));
+app.use(function (req, res, next) {
+	res.header(
+		'Access-Control-Allow-Methods',
+		'OPTIONS, HEAD, GET, PUT, POST, DELETE'
+	);
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get('/', (req, res) => {
-	console.log('test');
-	res.send({ data: 'Hello World!' });
+app.get('/bacon', (req, res) => {
+	res.status(200).json({
+		status: 200,
+		message: 'test',
+	});
 });
 
 app.listen(port, () => {
